61c88cdec5552299d4a0aebc2b23c6d8cf292f09
Rhyme Rubayet <rubayet211@gmail.com>
2024-07-17 16:18:59 +0600

    Successfully added Header

--- src/components/router-head/header/header.tsx
+++ src/components/router-head/header/header.tsx
@@ -1,10 +1,28 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useStyles$ } from "@builder.io/qwik";
 import { Link } from "@builder.io/qwik-city";
+import HeaderStyles from "./header.css?inline";
+import ImgBanana from "/public/banana.png?jsx";
 
 export default component$(() => {
+  useStyles$(HeaderStyles);
+
   return (
     <header>
-      <Link href="/about">About</Link>
+      <nav>
+        <ImgBanana />
+        <h1>Mar Khawa Life</h1>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 });
